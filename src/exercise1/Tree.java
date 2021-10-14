e4756dc1e67be0c5a6a8e6cc1f7fbe5042de2128
Deyvid <>
2021-10-14 05:25:32 +0000

    Files updated

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -3,7 +3,7 @@ package exercise1;
 public class Tree {
 
  public static void main(String[] args) {
-
+  /*
   for (int i = 0; i < 5; i++) {
     for (int j = 0; j < 5 - i; j++) {
       System.out.print(" ");
@@ -21,6 +21,15 @@ public class Tree {
       System.out.print("*");
     System.out.println();
   }
+
+*/
+
+System.out.println("    *");
+System.out.println("   ***");
+System.out.println("  *****");
+System.out.println(" *******");
+System.out.println("*********");
+System.out.println("   ***");
   
   }
 }
