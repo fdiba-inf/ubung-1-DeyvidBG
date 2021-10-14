2a4d8e4d5c3709a91fcd015ceef7528cf6b49932
Deyvid <>
2021-10-14 05:20:57 +0000

    Files updated

--- src/exercise1/Tree.java
+++ src/exercise1/Tree.java
@@ -5,7 +5,7 @@ public class Tree {
  public static void main(String[] args) {
 
   for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 10 - i; j++) {
+    for (int j = 0; j < 5 - i; j++) {
       System.out.print(" ");
     }
     for (int k = 0; k < (2 * i + 1); k++) {
@@ -15,7 +15,7 @@ public class Tree {
   }
   
   for (int i = 0; i < 1; i++) {
-    for (int j = 0; j < 9; j++)
+    for (int j = 0; j < 4; j++)
       System.out.print(" ");
     for (int k = 0; k < 3; k++)
       System.out.print("*");
